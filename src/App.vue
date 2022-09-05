2072872c9120a2c142bdfd145a11cc98529b6933
ekinkarada <fatrim53@gmail.com>
2022-09-05 21:00:30 +0300

    vue_add_product

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <h1 class="text-center">Ürün Ekleme Uygulaması</h1>
+    <hr>
+    <appNewProduct></appNewProduct>
+    <br><br>
+    <app-progress></app-progress>
+    <br><br>
+   
+    <app-products></app-products>
+    
+    
+
+  </div>
+
+
+</template>
+
+<script>
+import NewProduct from "./components/NewProduct.vue"
+import Progress from "./components/Progress.vue"
+import ProductComp from "./components/ProductsComp.vue"
+  export default {
+    components: {
+      appNewProduct : NewProduct,
+      appProgress : Progress,
+      appProducts: ProductComp
+    },
+
+  
+  }
+</script>
+
+<style>
+  body {
+    background-color: aliceblue;
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
+
+  .card{
+    margin-right: 5px;
+    margin-bottom: 5px;
+  }
+
+  .card:last-child{
+    margin-right: 0px;
+  }
+
+  .col-md-2{
+    max-width: 15.666667%!important;
+  }
+
+  .product-container{
+    margin-left: 15px;
+  }
+
+</style>
+
---
+++ src/components/Progress.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="progress">
+    <div class="progress-bar bg-info" role="progressbar" :style="{width:progress+'%'}" aria-valuenow="50"
+      aria-valuemin="0" aria-valuemax="100">
+      {{ itemCount }}/10
+    </div>
+  </div>
+</template>
+<script>
+import { eventBus } from "../main"
+export default {
+  data() {
+    return {
+      itemCount: 0,
+      progress: 0
+    }
+  },
+
+  created() {
+    eventBus.$on("progressBarUpdated", (productListCount) => {
+      this.itemCount = productListCount
+      this.progress = productListCount * 10
+    });
+  }
+}
+</script>
